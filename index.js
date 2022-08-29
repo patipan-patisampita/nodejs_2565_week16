465c0af1717e3644e65d2e47701164ca5606c759
patipan-patisampita <patipan.patisampita@gmail.com>
2022-08-29 16:44:18 +0700

    update

--- index.js
+++ index.js
@@ -9,21 +9,31 @@ const morgan = require('morgan')
 const PORT = process.env.PORT || 4000
 
 //database connection
-mongoose.connect(process.env.DB_URI,{useNewUrlParser:true})
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
 const db = mongoose.connection;
-db.on("error",(error)=>{
+db.on("error", (error) => {
     console.log(error)
 })
-db.once("open",()=>{
+db.once("open", () => {
     console.log("Connected to the databases")
 })
 
 app.use(morgan('dev'))
 
+//middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(session({ secret: "my secret key", saveUninitialized: true, resave: false }))
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
+
 //http://localhost:4000/
 app.get('/', (req, res) => {
     return res.json({
-        message:"Homepage"
+        message: "Homepage"
     })
 })
 
