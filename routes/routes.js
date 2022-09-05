9839d5817515d112b4008d89149ec2795603df58
patipan-patisampita <patipan.patisampita@gmail.com>
2022-09-05 16:20:19 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -17,8 +17,8 @@ const upload = multer({
     storage: storage,
 }).single("image")
 
-//Inser user into database route
-router.post('/add', (req, res) => {
+//Insert user into database route
+router.post('/add',upload,(req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
