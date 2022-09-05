20574e53ae674a8bb3d7a5529f472fe7b1b05ab9
patipan-patisampita <patipan.patisampita@gmail.com>
2022-09-05 15:35:10 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
     return res.render("index", { title: "Home Page" })
 })
 
+router.get("/add", (req, res) => {
+    res.render("add_users", { title: "Add Users" })
+})
+
 router.get('/users', (req, res) => {
     return res.status(200).json({
         message: "All Users"
