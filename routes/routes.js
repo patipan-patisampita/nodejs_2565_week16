0e0b63bfa1c478565d76740634ee1c2dabcd06b6
patipan-patisampita <patipan.patisampita@gmail.com>
2022-09-05 16:09:14 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const User = require("../models/users")
+
+//Image upload
+const storage = multer.diskStorage({
+    destination: function(req,res,cb){
+        cb(null,"./uploads")
+    },
+    filename: function(req,file,cb){
+        cb(null,file.fieldname + "_" + Date.now() + "_" + file.originalname)
+    }
+})
+
+const upload = multer({
+    storage:storage,
+}).single("image")
 
 router.get('/', (req, res) => {
     return res.render("index", { title: "Home Page" })
