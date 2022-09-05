8ce37eb4e444474849bea7646d020ff61e85f2d3
patipan-patisampita <patipan.patisampita@gmail.com>
2022-09-05 16:17:01 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -5,17 +5,40 @@ const User = require("../models/users")
 
 //Image upload
 const storage = multer.diskStorage({
-    destination: function(req,res,cb){
-        cb(null,"./uploads")
+    destination: function (req, res, cb) {
+        cb(null, "./uploads")
     },
-    filename: function(req,file,cb){
-        cb(null,file.fieldname + "_" + Date.now() + "_" + file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
     }
 })
 
 const upload = multer({
-    storage:storage,
+    storage: storage,
 }).single("image")
+
+//Inser user into database route
+router.post('/add', (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: req.file.filename,
+    })
+    user.save((err) => {
+        if (err) {
+            req.json({
+                message: err.message, type: "danger"
+            })
+        } else {
+            req.session.message = {
+                type: "success",
+                message: "User added successfully!"
+            }
+            res.redirect("/")
+        }
+    })
+})
 
 router.get('/', (req, res) => {
     return res.render("index", { title: "Home Page" })
